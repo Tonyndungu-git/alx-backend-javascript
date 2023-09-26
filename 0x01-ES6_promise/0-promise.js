2d10c23dccbee34cdcb2c27591370c5760141103
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-09-26 22:58:57 +0300

    es6 promise 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,20 +1,17 @@
 function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API request
+    const apiResponse = {
+      status: 200,
+      data: 'Sample data from API'
+    };
 
-
-    return new Promise((resolve, reject) =>{
-
-	const apiResponse = {
-	    status: 200,
-	    data: 'sample data from API'
-	};
-
-	if (apiResponse.status == 200) {
-	    resolve(apiResponse.data);
-	}else {
-	    reject(new Error('API request failed'));
-	}
-
-    });
-
+    if (apiResponse.status === 200) {
+      resolve(apiResponse.data);
+    } else {
+      reject(new Error('API request failed'));
+    }
+  });
 }
+
 export default getResponseFromAPI;
