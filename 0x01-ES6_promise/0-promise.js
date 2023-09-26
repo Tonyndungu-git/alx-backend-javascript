271f0f130d34a324f042d4c37a276cd217a2ba55
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-09-26 23:29:05 +0300

    correction 0n 0-promise.js and a function that returns a promise, parameter is a boolean

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an API request
-    const apiResponse = {
-      status: 200,
-      data: 'Sample data from API'
-    };
-
-    if (apiResponse.status === 200) {
-      resolve(apiResponse.data);
-    } else {
-      reject(new Error('API request failed'));
-    }
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
