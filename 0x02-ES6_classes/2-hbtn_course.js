b083f4496de557ce7cc85e38b60003c008863e58
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-09-28 22:55:48 +0300

    Import the class Currency from 3-currency.js, Implement a class named Pricing

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -12,7 +11,7 @@ export default class HolbertonCourse {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new Error('Name must be a string.');
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
@@ -23,7 +22,7 @@ export default class HolbertonCourse {
 
   set length(value) {
     if (typeof value !== 'number') {
-      throw new Error('Length must be a number.');
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
@@ -33,8 +32,11 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!Array.isArray(value) || value.some(item => typeof item !== 'string')) {
-      throw new Error('Students must be an array of strings.');
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
   }
