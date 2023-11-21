c1b7134afdb069daa2360c9e5dbdf41e21b43702
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-11-21 16:22:59 +0300

    Organizing a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,12 @@
+// Binds the routes to the appropriate handler in the given Express application.
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
